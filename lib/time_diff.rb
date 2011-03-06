a61e69f9b57232000800edac24f70fbe77fddfea
abhilash <abhidsm@gmail.com>
2011-03-06 18:11:30 +0530

    Added monkey patch method to Time class

--- lib/time_diff.rb
+++ lib/time_diff.rb
@@ -1,7 +1,8 @@
 require 'rubygems'
 require 'active_support/all'
-module TimeDiff
-  def diff(start_date, end_date)
+
+class Time
+  def self.diff(start_date, end_date)
     from_time = start_date.to_time if start_date.respond_to?(:to_time)
     to_time = end_date.to_time if end_date.respond_to?(:to_time)
     distance_in_seconds = ((to_time - from_time).abs).round
@@ -10,13 +11,8 @@ module TimeDiff
     %w(year month week day hour minute second).each do |interval|
         delta = (distance_in_seconds / 1.send(interval)).floor
         distance_in_seconds -= delta.send(interval)
-        components << pluralize(delta, interval)
+        components << delta
     end
-    return components
-  end
-  
-  def pluralize(value, text)
-    result = value > 1 ? text.pluralize : text
-    return "#{value} #{result}"
+    return {:year => components[0], :month => components[1], :week => components[2], :day => components[3], :hour => components[4], :minute => components[5], :second => components[6]}
   end
 end
--- test/test_time_diff.rb
+++ test/test_time_diff.rb
@@ -1,18 +1,17 @@
 require 'helper'
 require 'time'
-include TimeDiff
 
 class TestTimeDiff < Test::Unit::TestCase
   should "return the time differnce in displayable format" do
-    assert_test_scenarios(Time.parse('2011-03-06'), Time.parse('2011-03-07'), ["0 year", "0 month", "0 week", "1 day", "0 hour", "0 minute", "0 second"])
-    assert_test_scenarios(Time.parse('2011-03-06'), Time.parse('2011-04-08'), ["0 year", "1 month", "0 week", "3 days", "0 hour", "0 minute", "0 second"])
-    assert_test_scenarios(Time.parse('2011-03-06 12:30:00'), Time.parse('2011-03-07 12:30:30'), ["0 year", "0 month", "0 week", "1 day", "0 hour", "0 minute", "30 seconds"])
-    assert_test_scenarios(Time.parse('2011-03-06'), Time.parse('2013-03-07'), ["2 years", "0 month", "0 week", "1 day", "12 hours", "0 minute", "0 second"])
-    assert_test_scenarios(Time.parse('2011-03-06'), Time.parse('2011-03-14'), ["0 year", "0 month", "1 week", "1 day", "0 hour", "0 minute", "0 second"])
+    assert_test_scenarios(Time.parse('2011-03-06'), Time.parse('2011-03-07'), {:year => 0, :month => 0, :week => 0, :day => 1, :hour => 0, :minute => 0, :second => 0})
+    assert_test_scenarios(Time.parse('2011-03-06'), Time.parse('2011-04-08'), {:year => 0, :month => 1, :week => 0, :day => 3, :hour => 0, :minute => 0, :second => 0})
+    assert_test_scenarios(Time.parse('2011-03-06 12:30:00'), Time.parse('2011-03-07 12:30:30'), {:year => 0, :month => 0, :week => 0, :day => 1, :hour => 0, :minute => 0, :second => 30})
+    assert_test_scenarios(Time.parse('2011-03-06'), Time.parse('2013-03-07'), {:year => 2, :month => 0, :week => 0, :day => 1, :hour => 12, :minute => 0, :second => 0})
+    assert_test_scenarios(Time.parse('2011-03-06'), Time.parse('2011-03-14'), {:year => 0, :month => 0, :week => 1, :day => 1, :hour => 0, :minute => 0, :second => 0})
   end
 
   def assert_test_scenarios(start_date, end_date, expected_result)
-    date_diff = diff(start_date, end_date)
+    date_diff = Time.diff(start_date, end_date)
     assert_equal(date_diff, expected_result)
   end
 end
